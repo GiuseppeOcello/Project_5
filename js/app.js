31952a371b8b01791ef37cd3c94d34c4bc7ad645
giuseppe <ocello.giuseppe@gmail.com>
2021-07-29 14:55:57 +0300

    Project_5_Rev_02

--- js/app.js
+++ js/app.js
@@ -1,53 +1,56 @@
+//*** Lightbox script***//
 baguetteBox.run('.gallery');
 
-// const search = new Filter('search', 'data-caption');​
+window.addEventListener('load', function() {
+   baguetteBox.run('.gallery');
+ });
 
-// --- Search application --- //
-// Variables
+//*** Searching scripts***//
 
+//*** Variables
+const searchBar = document.querySelector(".search-bar");
 const search = document.querySelector("#search");
 const image = document.querySelectorAll('img');
 let wrapper = document.querySelectorAll('a');
-const searchValue = search.value.toUpperCase();
+let warningCaption = document.createElement('p');
+let warningImage = document.createElement('img');
+let checker = 0; // Used to know if there are any images with display property set to default
 
 
+//*** Functions
 
+/* Search Function, it compares the entered string at each keyup event with the
+data-caption attribute off the img elements and set the display property on and off 
+depending on the match*/
 
 search.addEventListener('keyup', e => {
-   // console.log(e.key);
-   console.log(search.value);
-   // console.log(imageCaption[0].getAttribute("data-caption"));
-   // console.log(imageCaption[0].textContent);
 
+
+   // Iterates thorugh all the img elements and compare the input with the caption
    for (i=0; i < image.length; i++) {
-      caption = wrapper[i].getAttribute("data-caption");
+      let caption = wrapper[i].getAttribute("data-caption");
          if (caption.toUpperCase().indexOf(search.value.toUpperCase()) > -1) {
             wrapper[i].style.display = "";
+            checker ++;
          } else {
             wrapper[i].style.display = "none";
+            checker += 0;
          }
-   }
-})
+         
+      }
 
+      /* It shows or hide a message if the checker variable is null or > 0, in other
+      words if some images are outputed from the previous loop of not */
+      if (checker === 0) {
+         warningCaption.className = "warningCpt";
+         warningCaption.textContent = "Sorry there is no match with your entry, try typing somthing else.";
+         warningImage.className = "warningImg";
+         warningImage.src ="img/thumbnails/Image_Not_Found.png";
+         searchBar.append(warningCaption, warningImage);
+      } else if (searchBar.contains(warningCaption)) {
+         searchBar.removeChild(warningCaption);
+         searchBar.removeChild(warningImage);
+      }
+      checker = 0;
+});
 
-
-
-// function myFunction() {
-//    // Declare variables
-//    var input, filter, ul, li, a, i, txtValue;
-//    input = document.getElementById('myInput');
-//    filter = input.value.toUpperCase();
-//    ul = document.getElementById("myUL");
-//    li = ul.getElementsByTagName('li');
- 
-//    // Loop through all list items, and hide those who don't match the search query
-//    for (i = 0; i < li.length; i++) {
-//      a = li[i].getElementsByTagName("a")[0];
-//      txtValue = a.textContent || a.innerText;
-//      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//        li[i].style.display = "";
-//      } else {
-//        li[i].style.display = "none";
-//      }
-//    }
-//  }
